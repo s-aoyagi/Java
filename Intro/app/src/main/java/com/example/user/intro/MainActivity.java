cc72ba9288f65f591f4cd1155dd33c5450508463
s-aoyagi <syougoaoyagi@gmail.com>
2016-05-05 22:35:17 +0900

    テキスト入力、ボタン追加

--- Intro/app/src/main/java/com/example/user/intro/MainActivity.java
+++ Intro/app/src/main/java/com/example/user/intro/MainActivity.java
@@ -2,6 +2,13 @@ package com.example.user.intro;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Button;
+import android.view.View.OnKeyListener;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button01=(Button)findViewById(R.id.button01);
+        button01.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                onEnter();
+            }
+        });
+
+        EditText editText01=(EditText)findViewById(R.id.editText01);
+        editText01.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                TextView textView01 = (TextView)findViewById(R.id.textView);
+                textView01.setText("入力された");
+                return false;
+            }
+        });
     }
 }
